23dbb41cefe8dd7f5399aec142e01ab3b0a492d5
Patrick Roberts <patrob22@gmail.com>
2023-03-26 22:05:06 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const findMatching = (drivers, string) => {
+    let filteredDrivers = drivers.filter((driver) => {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+    return filteredDrivers
+};
+
+const fuzzyMatch = (array, string) => {
+   let matchArray = array.filter((subj) => {
+        let cutPoint = subj.length - (subj.length - string.length);
+        return string === subj.slice(0, cutPoint);
+    });
+    return matchArray;
+};
+
+const matchName = (drivers, string) => {
+    let nameArray = drivers.filter((driver) => {
+        return string === driver.name;
+    });
+    return nameArray;
+};
